7cdad1649114297da3ad88c38a47f4bdf993a9eb
Michael Williamson <mike@zwobble.org>
2013-07-13 15:18:31 +0100

    Score of strike is ten plus next two throws

--- bowling/__init__.py
+++ bowling/__init__.py
@@ -1,2 +1,8 @@
 def score_game(game):
-    return sum(game)
+    total = 0
+    for index, pins_knocked_down in enumerate(game):
+        total += pins_knocked_down
+        if pins_knocked_down == 10:
+            total += game[index + 1] + game[index + 2]
+        
+    return total
--- tests/bowling_tests.py
+++ tests/bowling_tests.py
@@ -13,5 +13,8 @@ class BowlingTests(unittest.TestCase):
     def test_score_of_many_throws_is_total_number_of_pins_knocked_down(self):
         self.assert_game_score(14, [4, 2, 5, 3])
         
+    def test_score_of_strike_is_ten_plus_next_two_throws(self):
+        self.assert_game_score(21, [10, 2, 3, 1])
+        
     def assert_game_score(self, expected_score, game):
         self.assertEqual(expected_score, bowling.score_game(game))
