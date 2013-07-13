0e068dada5f4b23628aa6a5d17e90116f301636e
Michael Williamson <mike@zwobble.org>
2013-07-13 15:14:29 +0100

    Score of many throws is total number of pins knocked down

--- tests/bowling_tests.py
+++ tests/bowling_tests.py
@@ -9,3 +9,6 @@ class BowlingTests(unittest.TestCase):
 
     def test_score_of_one_throw_is_number_of_pins_knocked_down(self):
         self.assertEqual(4, bowling.score_game([4]))
+
+    def test_score_of_many_throws_is_total_number_of_pins_knocked_down(self):
+        self.assertEqual(14, bowling.score_game([4, 2, 5, 3]))
