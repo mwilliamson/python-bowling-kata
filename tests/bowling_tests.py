09f319f83d33f5ed1aaa808545c5935106e4d703
Michael Williamson <mike@zwobble.org>
2013-07-13 15:15:22 +0100

    Factor out test duplications by extracting assert_game_score function

--- tests/bowling_tests.py
+++ tests/bowling_tests.py
@@ -5,10 +5,13 @@ import bowling
 
 class BowlingTests(unittest.TestCase):
     def test_empty_game_has_score_of_zero(self):
-        self.assertEqual(0, bowling.score_game([]))
+        self.assert_game_score(0, [])
 
     def test_score_of_one_throw_is_number_of_pins_knocked_down(self):
-        self.assertEqual(4, bowling.score_game([4]))
+        self.assert_game_score(4, [4])
 
     def test_score_of_many_throws_is_total_number_of_pins_knocked_down(self):
-        self.assertEqual(14, bowling.score_game([4, 2, 5, 3]))
+        self.assert_game_score(14, [4, 2, 5, 3])
+        
+    def assert_game_score(self, expected_score, game):
+        self.assertEqual(expected_score, bowling.score_game(game))
