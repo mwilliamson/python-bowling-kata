1080621ff613b6b90b885d50e04fd112882461d4
Michael Williamson <mike@zwobble.org>
2013-07-13 15:34:42 +0100

    Separate calculation of frames from calculation of score

--- bowling/__init__.py
+++ bowling/__init__.py
@@ -1,19 +1,24 @@
 def score_game(throws):
     game = Game(throws)
     total = 0
-    index = 0
-    while index < len(game):
+    for index in _frame_indexes(game):
         total += game.pins_knocked_down(index) + game.pins_knocked_down(index + 1)
         if _is_strike(game, index):
             total += game.pins_knocked_down(index + 2)
-            index += 1
         elif _is_spare(game, index):
             total += game.pins_knocked_down(index + 2)
-            index += 2
-        else:
-            index += 2
         
     return total
+
+
+def _frame_indexes(game):
+    index = 0
+    while index < len(game):
+        yield index
+        if _is_strike(game, index):
+            index += 1
+        else:
+            index += 2
 
 
 def _is_strike(game, index):
